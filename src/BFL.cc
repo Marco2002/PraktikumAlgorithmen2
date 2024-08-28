afb2c2b93cb989b1999c92e75e1f26bdea521de0
Marco <marcoclone@gmail.com>
2024-08-28 16:12:03 +0200

    add reachability query

--- src/BFL.cc
+++ src/BFL.cc
@@ -1,34 +1,32 @@
 #include "BFL.h"
 
 #include <functional>
-#include <bitset>
 
-void depth_first_search_visit(const node& n, std::vector<long>& discover_label, std::vector<long>& finish_label, std::vector<const node*>& post_order, long& current) {
-    discover_label[n.index_] = ++current;
+void depth_first_search_visit(const node& n, LabelDiscovery& label_discover, LabelFinish& label_finish, std::vector<const node*>& post_order, long& current) {
+    label_discover[n.index_] = ++current;
     for (auto const e : n.outgoing_edges_) {
-        if (discover_label[e->index_] == 0) { // if e was not visited
-            depth_first_search_visit(*e, discover_label, finish_label, post_order, current);
-        }
+        if (label_discover[e->index_] != 0) continue; // if e was visited
+
+        depth_first_search_visit(*e, label_discover, label_finish, post_order, current);
     }
     post_order.push_back(&n);
-    finish_label[n.index_] = ++current;
+    label_finish[n.index_] = ++current;
 
 }
 
-std::tuple<std::vector<const node*>, std::vector<long>, std::vector<long>> depth_first_search(const graph& g) {
-    // TODO check if discovery_label and finish_label are necessary
-    std::vector<long> discovery_label(g.nodes_.size());
-    std::vector<long> finish_label(g.nodes_.size());
+std::tuple<std::vector<const node*>, LabelDiscovery, LabelFinish> depth_first_search(const graph& g) {
+    LabelDiscovery label_discovery(g.nodes_.size());
+    LabelFinish label_finish(g.nodes_.size());
     std::vector<const node*> post_order = {};
     long current = 0;
 
     for(auto n : g.nodes_) {
         if(n->incoming_edges_.empty()) {
-            depth_first_search_visit(*n, discovery_label, finish_label, post_order, current);
+            depth_first_search_visit(*n, label_discovery, label_finish, post_order, current);
         }
     }
 
-    return std::make_tuple(post_order, discovery_label, finish_label);
+    return std::make_tuple(post_order, label_discovery, label_finish);
 }
 
 std::vector<const node*> merge_vertices(const std::vector<const node*>& post_order, const long d) {
@@ -67,7 +65,7 @@ void compute_label_out(const graph& graph, std::vector<node*>& g, const std::fun
 }
 
 template <size_t hash_range>
-void compute_label_in(const graph& graph, std::vector<node*>& g, const std::function<int(node*)>& h, const node& n, std::vector<std::bitset<hash_range>>& label_in) {
+void compute_label_in(const graph& graph, std::vector<node*>& g, const std::function<int(node*)>& h, const node& n, LabelIn<hash_range>& label_in) {
     label_in[n.index_].set(h(g[n.index_]));
     for(auto const predecessor : n.incoming_edges_) {
         if(label_in[predecessor->index_].none()) { // if successor has not been visited
@@ -79,18 +77,40 @@ void compute_label_in(const graph& graph, std::vector<node*>& g, const std::func
 
 // the hash should map to values in a range from 0...hash_range-1
 template <size_t hash_range> // the range is the number of values that can be possible outputs of the hash function
-void build_bfl_index(const graph& graph, const std::function<int(node*)>& h, const long d) {
-    std::vector<std::bitset<hash_range>> label_out(graph.nodes_.size());
-    std::vector<std::bitset<hash_range>> label_in(graph.nodes_.size());
+BflLabels<hash_range> build_bfl_index(const graph& graph, const std::function<int(node*)>& h, const long d) {
+    LabelIn<hash_range> label_in(graph.nodes_.size());
+    LabelOut<hash_range> label_out(graph.nodes_.size());
 
-    auto [post_order, discovery_label, finish_label] = depth_first_search(graph);
+    auto [post_order, label_discovery, label_finish] = depth_first_search(graph);
     auto g = merge_vertices(post_order, d);
 
     for(auto n : post_order) {
         if(label_out[n->index_].none()) {
-            compute_label_out<hash_range>(graph, g, h, *n, label_out);
             compute_label_in<hash_range>(graph, g, h, *n, label_in);
+            compute_label_out<hash_range>(graph, g, h, *n, label_out);
         }
     }
+
+    return std::make_tuple(label_discovery, label_finish, label_in, label_out);
 }
 
+template <size_t hash_range>
+bool query_reachability(const graph& graph, BflLabels<hash_range> labels, const node& u, const node& v, std::vector<bool>& visited) {
+    visited[u.index_] = true;
+
+    auto const [label_discovery, label_finish, label_in, label_out] = labels;
+    if(label_discovery[u.index_] <= label_discovery[v.index_] && label_finish[v.index_] <= label_finish[u.index_]) {
+        return true;
+    }
+    if((label_out[v] & label_out[u] != label_out[v]) || (label_in[u] & label_in[v] != label_in[u])) { // if L_out(v) !subset_of L_out(u) or L_in(u) !subset_of L_in(v)
+        return false;
+    }
+    for(auto const w : u.outgoing_edges_) {
+        if(visited[w->index_]) continue;
+
+        if(query_reachability<hash_range>(graph, labels, *w, v, visited)) {
+            return true;
+        }
+    }
+    return false;
+}
--- src/BFL.h
+++ src/BFL.h
@@ -1,6 +1,17 @@
 #include "graphs.h"
 
-#include <unordered_map>
+#include <bitset>
 
 using namespace graphs;
 
+template <size_t hash_range>
+using LabelIn = std::vector<std::bitset<hash_range>>;
+
+template <size_t hash_range>
+using LabelOut = std::vector<std::bitset<hash_range>>;
+
+using LabelDiscovery = std::vector<long>;
+using LabelFinish = std::vector<long>;
+
+template <size_t hash_range>
+using BflLabels = std::tuple<LabelDiscovery, LabelFinish, LabelIn<hash_range>, LabelOut<hash_range>>;
